1d8b8579037a5cdbacd8c851c8d56c66368ab21d
jones58 <108466184+jones58@users.noreply.github.com>
2024-03-11 12:09:14 +0000

    edit to json format and import into home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,12 @@
+<script setup>
+import MovieList from "../MovieList.json";
+console.log(MovieList);
+</script>
+
 <template>
   <main class="text-3xl font-bold underline text-black">Hello</main>
+  <div v-for="item in MovieList" :key="movieIndex">
+    {{ item.Name }}
+  </div>
 </template>
+../MovieList
