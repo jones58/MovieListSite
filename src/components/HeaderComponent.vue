7fb6fce0afbec385cc54f7de8b6c56f41ed7c398
jones58 <108466184+jones58@users.noreply.github.com>
2024-03-11 12:23:49 +0000

    split header to separate component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <header
+    class="bg-indigo-800 text-gray-100 text-xl flex justify-between p-5 w-full text-3xl"
+  >
+    <router-link class="" to="/">MovieList</router-link>
+    <router-link class="" to="/about">About</router-link>
+  </header>
+</template>
